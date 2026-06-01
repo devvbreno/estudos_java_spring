d07be66a76d94cc9324102d0d296724817914360
Breno Elias <brenoelias05@gmail.com>
2026-06-01 18:43:17 -0300

    Implementando sobrecarga e encapsulamento

--- exercicios-java-udemy/src_stock_product/entities/Product.java
+++ exercicios-java-udemy/src_stock_product/entities/Product.java
@@ -2,18 +2,45 @@ package src_.entities;
 
 public class Product {
 
-        public String name;
-        public double price;
-        public int quantity;
+        private String name;
+        private double price;
+        private int quantity;
 
         public Product(String name, double price, int quantity) {
             this.name = name;
             this.price = price;
             this.quantity = quantity;
-            
         }
-    
-    public double TotalValueInStock() {
+        public Product() {
+
+        }
+        public Product(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+        
+        public String getName(){
+            return name;
+        }
+
+        public void setName (String name) {
+            this.name = name; 
+        }
+        
+        public double getPrice(){
+            return price;
+        }
+
+        public void setPrice (double price) {
+            this.price = price; 
+        }
+
+        public int getQuantity(){
+            return quantity;
+        }
+
+
+    public double totalValueInStock() {
         return price * quantity; 
     }
     public void addProduct(int quantity) {
@@ -21,5 +48,15 @@ public class Product {
     }
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
+    }
+
+    public String toString(){
+        return name 
+            + ", $" 
+            + String.format("%.2f", price) 
+            + ", " 
+            + quantity 
+            + " units, Total: $ "
+            + String.format("%.2f", totalValueInStock());
     }
 }
